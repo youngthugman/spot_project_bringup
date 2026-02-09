ad96f5de5d1ac596f11a34bb26caac41c73a605f
Max <your_email@example.com>
2026-02-09 17:34:40 +1100

    Fixed how urdf generates on spot, spot now starts up from a single startup file including rviz slamtoolbox and models. Need to fix how lidar sits atop spot

--- src/my_spot_bringup/launch/bringup_slam.launch.py
+++ src/my_spot_bringup/launch/bringup_slam.launch.py
@@ -3,23 +3,32 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
-
+import os
+from launch.actions import SetEnvironmentVariable
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 # Runs spot driver launch py to execute driver nodes
+    
+    set_extras = SetEnvironmentVariable(
+        name='SPOT_URDF_EXTRAS',
+        value='/home/max/spot_ws/src/spot_project_bringup/src/my_spot_bringup/urdf_extras/Spot_2D_Lidar.urdf'
+    )
+
+
     spot_driver_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            [
+            os.path.join(
                 get_package_share_directory('spot_driver'),
-                '/launch/spot_driver.launch.py'
-            ]
+                'launch',
+                'spot_driver.launch.py'
+            )
         ),
         launch_arguments={
             'config_file': '/home/max/spot_ws/src/spot_ros2/spot_driver/config/spot_ros_example.yaml'
         }.items(),
-    )
+)
 
     urg_node = Node(
         package='urg_node',
@@ -27,18 +36,6 @@ def generate_launch_description():
         name='urg_node',
         output='screen',
         parameters=['/home/max/spot_ws/src/spot_project_bringup/config/urg_node_serial.yaml'],
-    )
-    #Puts the urdf into text for the robot_description to run off.
-    my_spot_lidar_mount_path = "/home/max/spot_ws/src/spot_project_bringup/src/my_spot_description/urdf/spot_lidar_mount.urdf"
-    with open(my_spot_lidar_mount_path,"r" ) as text:
-        robot_description = text.read()
-    robot_state_publisher = Node(
-        package ="robot_state_publisher",
-        executable="robot_state_publisher",
-        name="robot_state_publisher" ,
-        output="screen" ,
-        parameters=[{"robot_description": robot_description}],
-
     )
 
     #Need to change frame id: laser to use this not laser_link.
@@ -65,13 +62,12 @@ def generate_launch_description():
         output='screen',
         arguments=['-d', '/home/max/.rviz2/spotrviz.rviz'],
     )
+    ld.add_action(set_extras)
     ld.add_action(spot_driver_launch)
     ld.add_action(urg_node)
-    ld.add_action(robot_state_publisher)
     #ld.add_action(static_tf_body_to_laser)
     ld.add_action(slam_toolbox)
     ld.add_action(rviz)
-
     return ld
 
 
--- src/my_spot_bringup/setup.py
+++ src/my_spot_bringup/setup.py
@@ -1,5 +1,6 @@
 from setuptools import find_packages, setup
-
+from glob import glob
+from pathlib import Path
 package_name = 'my_spot_bringup'
 
 setup(
@@ -10,6 +11,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
